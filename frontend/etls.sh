a037f48aad8980401c71f72ff94a46a91ee70acb
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-08-03 11:55:22 +0300

    Add etl helper script

---
+++ frontend/etls.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Perform the DELETE request
+curl -X DELETE "https://re-cycler-three.vercel.app/api/etl/collection_spots"
+
+# Function to perform POST requests in the specified range
+perform_post_requests() {
+  local pageSize=5
+  local start=$1
+  local end=$2
+  for ((i=start; i<=end; i+=pageSize)); do
+    from=$i
+    to=$((i + pageSize - 1))
+    curl -X POST "https://re-cycler-three.vercel.app/api/etl/collection_spots?from=$from&to=$to"
+  done
+}
+
+# Perform POST requests in the specified range
+perform_post_requests 1 200
