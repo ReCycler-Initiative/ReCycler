9712b5efa677b1e94beb007675c9a49107a15b98
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-08-04 11:29:15 +0300

    Fix materials order by

--- frontend/src/app/api/materials/route.ts
+++ frontend/src/app/api/materials/route.ts
@@ -2,6 +2,6 @@ import db from "@/services/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const result = await db("recycler.materials").orderBy("material_name");
+  const result = await db("recycler.materials").orderBy("name");
   return NextResponse.json(result);
 }
