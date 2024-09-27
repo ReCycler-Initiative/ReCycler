1f98ec65511bd29295709af6e1b957b420368b0f
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-09-27 16:15:07 +0300

    Add loading spinner to materials

---
+++ frontend/src/components/loading-spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="animate-spin h-8 w-8 border-2 border-t-transparent border-gray-500 rounded-full"></div>
+  );
+};
+
+export default LoadingSpinner;
