bc09708a9dfd9128e256a678cf5bcbb16bd08f96
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-10-09 13:20:44 +0300

    Test Python function

---
+++ backend/api/vercel/api/ping.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler
+ 
+class handler(BaseHTTPRequestHandler):
+ 
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        response_body = "Hello, this is the response body!"
+        self.wfile.write(response_body.encode('utf-8')) 
+
+        return
