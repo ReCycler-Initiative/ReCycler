92f49dae715ab5bf6e1b27ff55fbe09ac9c6bc50
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2024-10-25 14:34:02 +0300

    Remove pop over

--- frontend/src/components/materials.tsx
+++ frontend/src/components/materials.tsx
@@ -1,60 +1,29 @@
 import { getMaterials } from "@/services/api";
 import { useQuery } from "@tanstack/react-query";
-import { InfoIcon, Paperclip, RecycleIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { RecycleIcon } from "lucide-react";
+import { useState } from "react";
 import { useFormContext } from "react-hook-form";
 import LoadingSpinner from "./loading-spinner";
-import { Button } from "./ui/button";
-import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 const CustomCheckbox = ({ label, name }: { label: string; name: string }) => {
   const { register, watch } = useFormContext();
   const checked: boolean = watch(name);
 
   return (
-    <div className="relative">
-      <label
-        data-checked={checked}
-        className="border flex-col border-gray-700 py-2 px-2 justify-center text-center aspect-square flex items-center bg-white rounded-sm data-[checked=true]:bg-gray-700 data-[checked=true]:text-white"
-      >
-        <RecycleIcon className="mb-3 text-red-600" />
-        <input
-          {...register(name)}
-          checked={checked || false}
-          className="hidden"
-          type="checkbox"
-        />
-        <span className="checkbox-mark"></span>
-        <span className="text-sm">{label}</span>
-      </label>
-      <Popover modal>
-        <PopoverTrigger asChild>
-          <Button
-            className="text-gray-600 absolute bottom-[1px] right-[1px]"
-            size="icon"
-            variant="ghost"
-          >
-            <InfoIcon className="text-gray-500" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="text-sm" align="center" sideOffset={5}>
-          <h4 className="mb-4 font-bold text-base">Lamput</h4>
-          <p className="text-green-600">Kyllä</p>
-          <ul className="list-disc py-2 px-4 mb-2">
-            <li>Energiansäästölamput</li>
-            <li>Loisteputket</li>
-          </ul>
-          <p className="text-red-600">Ei</p>
-          <ul className="list-disc py-2 px-4">
-            <li>halogeenilamput (kodin jäteastiaan)</li>
-            <li>hehkulamput (kodin jäteastiaan)</li>
-            <li>autojen lamput (kodin jäteastiaan)</li>
-          </ul>
-
-          <span className="block mt-4 underline text-right">Lue lisää</span>
-        </PopoverContent>
-      </Popover>
-    </div>
+    <label
+      data-checked={checked}
+      className="border flex-col border-gray-700 py-2 px-2 justify-center text-center aspect-square flex items-center bg-white rounded-sm data-[checked=true]:bg-gray-700 data-[checked=true]:text-white"
+    >
+      <RecycleIcon className="mb-3 text-red-600" />
+      <input
+        {...register(name)}
+        checked={checked || false}
+        className="hidden"
+        type="checkbox"
+      />
+      <span className="checkbox-mark"></span>
+      <span className="text-sm">{label}</span>
+    </label>
   );
 };
 
