bfee5660816d5362d747f910e1b6f066ce5e3ee2
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-02-26 20:56:09 +0200

    Use Zod to parse params

--- frontend/src/app/api/organizations/[organizationId]/locations/route.ts
+++ frontend/src/app/api/organizations/[organizationId]/locations/route.ts
@@ -1,22 +1,40 @@
 import db from "@/services/db";
 import { NextResponse } from "next/server";
+import { z, ZodError } from "zod";
+
+const GetLocationsRequest = z.object({
+  organizationId: z.string().uuid(),
+});
+
+type TGetLocationRequest = z.infer<typeof GetLocationsRequest>;
 
 export async function GET(
   _: Request,
-  { params }: { params: { organizationId: string } }
+  { params }: { params: TGetLocationRequest }
 ) {
-  const { organizationId } = params;
+  try {
+    const { organizationId } = GetLocationsRequest.parse(await params);
 
-  if (!organizationId) {
+    if (!organizationId) {
+      return NextResponse.json(
+        { error: "organisation_id is required" },
+        { status: 400 }
+      );
+    }
+
+    const result = await db.raw(`SELECT * FROM recycler.get_locations(?);`, [
+      organizationId,
+    ]);
+
+    return NextResponse.json(result.rows);
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json({ error: error.errors }, { status: 400 });
+    }
+
     return NextResponse.json(
-      { error: "organisation_id is required" },
-      { status: 400 }
+      { error: "Something went wrong" },
+      { status: 500 }
     );
   }
-
-  const result = await db.raw(`SELECT * FROM recycler.get_locations(?);`, [
-    organizationId,
-  ]);
-
-  return NextResponse.json(result.rows);
 }
