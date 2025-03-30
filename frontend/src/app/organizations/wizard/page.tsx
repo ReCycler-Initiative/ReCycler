ce16011ca78f5d1bfe8e1d1a17bdbc1d4fa9d891
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-03-30 09:38:39 +0300

    Add welcome step

--- frontend/src/app/organizations/wizard/page.tsx
+++ frontend/src/app/organizations/wizard/page.tsx
@@ -6,7 +6,6 @@ import { Form } from "@/components/ui/form";
 import { LocationProperties, Organization } from "@/types";
 import { useState } from "react";
 import {
-  DefaultValues,
   FieldValues,
   useFieldArray,
   useForm,
@@ -14,34 +13,39 @@ import {
 } from "react-hook-form";
 import { z } from "zod";
 
-type StepProps<T extends FieldValues> = {
+type StepProps = {
+  children: React.ReactNode;
+  title: string;
+};
+
+function Step({ children, title }: StepProps) {
+  return (
+    <div className="p-10 mx-auto w-full max-w-xl">
+      <h1 className="text-3xl mb-6">{title}</h1>
+      {children}
+    </div>
+  );
+}
+
+type StepFormProps<T extends FieldValues> = {
   children: React.ReactNode;
   form: UseFormReturn<T, any, undefined>;
   onStepChange: (values: T) => void;
-  title: string;
-  values: DefaultValues<T>;
 };
 
-function Step<T extends FieldValues>({
+function StepForm<T extends FieldValues>({
   children,
   form,
   onStepChange,
-  title,
-  values,
-}: StepProps<T>) {
-  const form = useForm<T>({
-    defaultValues: values,
-  });
-
+}: StepFormProps<T>) {
   return (
     <Form {...form}>
       <form
-        className="p-10 mx-auto w-full max-w-xl"
+        className="contents"
         onSubmit={form.handleSubmit((values) => {
           onStepChange(values);
         })}
       >
-        <h1 className="mb-6">{title}</h1>
         {children}
       </form>
     </Form>
@@ -53,6 +57,21 @@ type TOrganization = z.infer<typeof Organization>;
 type FullState = {
   fields: Field[];
   organization: TOrganization;
+};
+
+const WelcomeStep = ({ onStepChange }: { onStepChange: () => void }) => {
+  return (
+    <Step title="Tervetuloa">
+      <p>
+        Tämän vaiheittaisen opastuksen avulla voit luoda oman tilin ja
+        määritellä, millaisia tietoja haluat tallentaa eri kohteista. Aloitetaan
+        ensimmäisestä vaiheesta klikkaamalla {'"Seuraava"'}.
+      </p>
+      <div className="pt-6 flex justify-end">
+        <Button onClick={onStepChange}>Seuraava</Button>
+      </div>
+    </Step>
+  );
 };
 
 const OrganizationStep = ({
@@ -67,16 +86,18 @@ const OrganizationStep = ({
   });
 
   return (
-    <Step form={form} onStepChange={onStepChange} title="Organisaatio">
-      <FormInput
-        label="Nimi"
-        name="name"
-        rules={{ required: "Nimi on pakollinen" }}
-      />
-      <div className="pt-6 flex justify-end">
-        <Button>Seuraava</Button>
-      </div>
-    </Step>
+    <StepForm form={form} onStepChange={onStepChange}>
+      <Step title="Organisaatio">
+        <FormInput
+          label="Nimi"
+          name="name"
+          rules={{ required: "Nimi on pakollinen" }}
+        />
+        <div className="pt-6 flex justify-end">
+          <Button>Seuraava</Button>
+        </div>
+      </Step>
+    </StepForm>
   );
 };
 
@@ -99,11 +120,13 @@ const LocationFieldsModel = ({ values }: { values: Field[] }) => {
   });
 
   return (
-    <Step form={form} onStepChange={() => undefined} title="Kohteen kentät">
-      <div className="pt-6 flex justify-end">
-        <Button type="submit">Seuraava</Button>
-      </div>
-    </Step>
+    <StepForm form={form} onStepChange={() => undefined}>
+      <Step title="Kohteen kentät">
+        <div className="pt-6 flex justify-end">
+          <Button type="submit">Seuraava</Button>
+        </div>
+      </Step>
+    </StepForm>
   );
 };
 
@@ -117,6 +140,10 @@ const Wizard = () => {
   });
 
   if (step === "step1") {
+    return <WelcomeStep onStepChange={() => setStep("step2")} />;
+  }
+
+  if (step === "step2") {
     return (
       <OrganizationStep
         onStepChange={() => {
