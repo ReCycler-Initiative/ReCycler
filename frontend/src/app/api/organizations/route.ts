4d631587ea1ec93833eff1f2590dbff044783709
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-04-23 16:43:44 +0300

    Add support to store org and fields to db.

--- frontend/src/app/api/organizations/route.ts
+++ frontend/src/app/api/organizations/route.ts
@@ -1,8 +1,31 @@
+import knex from "@/services/db";
 import { CreateOrganizationRequest } from "@/types";
 import { withZodPost } from "@/utils/routes";
 import { NextResponse } from "next/server";
 
 export const POST = withZodPost(CreateOrganizationRequest, async (params) => {
+  const { organization, fields } = params;
+
+  knex.insert(organization);
+
+  await knex.transaction(async (trx) => {
+    const [row] = await trx
+      .insert(organization)
+      .into("recycler.organizations")
+      .returning("id");
+
+    await Promise.all(
+      fields.map((field) =>
+        trx
+          .insert({
+            ...field,
+            organization_id: row.id,
+          })
+          .into("recycler.fields")
+      )
+    );
+  });
+
   return NextResponse.json({
     message: "Organization created",
   });
