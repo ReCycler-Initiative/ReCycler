0d646beb91ea326c67f4784331ef4995de0ef929
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-04-23 17:02:34 +0300

    Add thank you page scaffolding

---
+++ frontend/src/app/organizations/wizard/thankyou/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+
+const ThankYou = () => {
+  return (
+    <div>
+      <h1 className="text-2xl text-center py-6 border-b bg-gray-50 text-primary">
+        Organisaatio luotiin onnistuneesti
+      </h1>
+    </div>
+  );
+};
+
+export default ThankYou;
