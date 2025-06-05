b755372951abf4cc00c1b76d7a3f0bef39cf4bf0
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-06-05 08:22:46 +0300

    Make configurable home href

--- frontend/src/app/admin/organizations/[id]/layout.tsx
+++ frontend/src/app/admin/organizations/[id]/layout.tsx
@@ -1,6 +1,5 @@
 import TitleBar from "@/components/title-bar";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 
 const AdminLayout = async ({
   children,
@@ -12,7 +11,7 @@ const AdminLayout = async ({
   const { id } = await params;
   return (
     <div className="flex flex-col h-full">
-      <TitleBar>
+      <TitleBar toHomeHref={`/admin/organizations/${id}`}>
         <Link href={`${id}/general_info`}>General Info</Link>
       </TitleBar>
       <main className="flex-1 flex flex-col bg-gray-100">{children}</main>
--- frontend/src/components/title-bar.tsx
+++ frontend/src/components/title-bar.tsx
@@ -3,13 +3,19 @@ import Link from "next/link";
 import logo from "../app/recycler-logo.png";
 import { ReactNode } from "react";
 
-const TitleBar = ({ children }: { children: ReactNode }) => {
+const TitleBar = ({
+  children,
+  toHomeHref = "/",
+}: {
+  children?: ReactNode;
+  toHomeHref?: string;
+}) => {
   return (
-    <header className="pb-2 pl-1 border-b border-gray-400 sticky top-0 bg-white shadow-md z-50">
-      <Link href="/">
+    <header className="pb-2 pl-1 border-b border-gray-400 sticky top-0 bg-white shadow-md z-50 flex items-center">
+      <Link href={toHomeHref}>
         <Image src={logo} alt="Recycler logo" width={150} />
       </Link>
-      {children && <nav>{children}</nav>}
+      {children && <nav className="pt-2 px-12">{children}</nav>}
     </header>
   );
 };
