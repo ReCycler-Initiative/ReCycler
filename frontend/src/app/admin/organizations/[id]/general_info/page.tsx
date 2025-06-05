6a634e9fbda5f4fd0b0a0ed7cdeadf8afc7e1abd
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-06-05 08:19:25 +0300

    Add scaffolding for general info of org page

---
+++ frontend/src/app/admin/organizations/[id]/general_info/page.tsx
@@ -0,0 +1,11 @@
+const GeneralInfoPage = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">General Information</h1>
+      <p className="text-gray-600">
+        This page contains general information about the organization.
+      </p>
+    </div>
+  );
+};
+export default GeneralInfoPage;
