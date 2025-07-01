af9436986880de877c59dd19aa7c603dd2f62654
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-07-01 17:36:23 +0300

    Remove chat services

--- app/src/app/api/chat/route.ts
+++
@@ -1,9 +0,0 @@
-import * as chat from "@/services/chatbot";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function POST(request: NextRequest) {
-  const body = await request.json();
-  const response = await chat.chat(body.message);
-
-  return NextResponse.json({ message: response });
-}
--- app/src/services/chatbot.ts
+++
@@ -1,6 +0,0 @@
-import db from "@/services/db";
-
-export const chat = (message: string): Promise<string> =>
-  db
-    .raw("SELECT generate_rag_response(?)", [message])
-    .then((res) => res.rows[0].generate_rag_response);
