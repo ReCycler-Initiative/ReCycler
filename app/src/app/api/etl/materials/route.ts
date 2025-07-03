8f4cabf70dd8a080ba869378bbea175645920045
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-07-03 14:13:36 +0300

    Remove reading ai training materials for now

--- app/src/app/api/etl/materials/route.ts
+++ app/src/app/api/etl/materials/route.ts
@@ -1,51 +1,49 @@
 import db from "@/services/db";
 import axios from "axios";
 import { Knex } from "knex";
-import fs from "fs";
-import path from "path";
 
-const filenameCodeMap: { [key: string]: number } = {
-  "Ajoneuvoakut(lyijy).txt": 115,
-  "energiajäte.txt": 102,
-  "kartonki.txt": 105,
-  "kyllästetty-puu.txt": 118,
-  "lamput.txt": 116,
-  "lasi.txt": 107,
-  "metalli.txt": 106,
-  "muovi.txt": 111,
-  "muujäte.txt": 114,
-  "pahvi.txt": 104,
-  "paperi.txt": 103,
-  "Paristot-ja-pienakut.txt": 110,
-};
+// const filenameCodeMap: { [key: string]: number } = {
+//   "Ajoneuvoakut(lyijy).txt": 115,
+//   "energiajäte.txt": 102,
+//   "kartonki.txt": 105,
+//   "kyllästetty-puu.txt": 118,
+//   "lamput.txt": 116,
+//   "lasi.txt": 107,
+//   "metalli.txt": 106,
+//   "muovi.txt": 111,
+//   "muujäte.txt": 114,
+//   "pahvi.txt": 104,
+//   "paperi.txt": 103,
+//   "Paristot-ja-pienakut.txt": 110,
+// };
 
-async function readFilesFromFolder(folderPath: string) {
-  const fileContents: { [key: number]: string } = {};
+// async function readFilesFromFolder(folderPath: string) {
+//   const fileContents: { [key: number]: string } = {};
 
-  try {
-    const files = await fs.promises.readdir(folderPath);
+//   try {
+//     const files = await fs.promises.readdir(folderPath);
 
-    for (const filename in filenameCodeMap) {
-      if (!files.includes(filename)) {
-        throw new Error(`File ${filename} not found in folder ${folderPath}`);
-      }
-    }
+//     for (const filename in filenameCodeMap) {
+//       if (!files.includes(filename)) {
+//         throw new Error(`File ${filename} not found in folder ${folderPath}`);
+//       }
+//     }
 
-    for (const filename of files) {
-      if (filename in filenameCodeMap) {
-        const filePath = path.join(folderPath, filename);
-        const data = await fs.promises.readFile(filePath, "utf8");
-        const code = filenameCodeMap[filename];
-        fileContents[code] = data;
-      }
-    }
-  } catch (err) {
-    console.error("Error reading directory or files:", err);
-    throw err;
-  }
+//     for (const filename of files) {
+//       if (filename in filenameCodeMap) {
+//         const filePath = path.join(folderPath, filename);
+//         const data = await fs.promises.readFile(filePath, "utf8");
+//         const code = filenameCodeMap[filename];
+//         fileContents[code] = data;
+//       }
+//     }
+//   } catch (err) {
+//     console.error("Error reading directory or files:", err);
+//     throw err;
+//   }
 
-  return fileContents;
-}
+//   return fileContents;
+// }
 
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
 
@@ -58,7 +56,7 @@ export function GET(request: Request) {
 }
 
 export async function POST() {
-  const contents = await readFilesFromFolder("../ai-training-materials");
+  // const contents = await readFilesFromFolder("../ai-training-materials");
 
   try {
     await db.transaction(async (trx: Knex.Transaction) => {
@@ -66,7 +64,9 @@ export async function POST() {
       await db("recycler.materials").delete();
 
       // Fetch materials from the API
-      const response = await axios.get(`${baseUrl}/materialtypes/?api_key=${apiKey}`);
+      const response = await axios.get(
+        `${baseUrl}/materialtypes/?api_key=${apiKey}`
+      );
 
       if (response.status === 200) {
         const data = response.data;
@@ -74,7 +74,7 @@ export async function POST() {
           (material: { code: number; name: string }) => ({
             code: material.code,
             name: material.name,
-            contents: contents[material.code] ?? material.name,
+            // contents: contents[material.code] ?? material.name,
           })
         );
 
