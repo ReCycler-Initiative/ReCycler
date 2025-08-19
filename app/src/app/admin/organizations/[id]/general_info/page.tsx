9a0eb5f7099084d47a833ec0c148007e012d71b3
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-08-19 21:11:52 +0300

    Change org info page title

--- app/src/app/admin/organizations/[id]/general_info/page.tsx
+++ app/src/app/admin/organizations/[id]/general_info/page.tsx
@@ -2,7 +2,7 @@ import { PageTemplate } from "@/components/admin/page-template";
 
 const GeneralInfoPage = () => {
   return (
-    <PageTemplate title="General Information">
+    <PageTemplate title="Organization Information">
       <p className="text-gray-600">
         This page contains general information about the organization.
       </p>
