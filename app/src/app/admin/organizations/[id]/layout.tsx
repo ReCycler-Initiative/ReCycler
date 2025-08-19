9660d0bb9d970c5df00f55a8e2cf2b427e5b28b5
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-08-19 20:12:40 +0300

    Move use case to the right

--- app/src/app/admin/organizations/[id]/layout.tsx
+++ app/src/app/admin/organizations/[id]/layout.tsx
@@ -30,8 +30,14 @@ const AdminLayout = ({
   return (
     <div className="flex flex-col h-full">
       <TitleBar toHomeHref={`/admin/organizations/${id}`}>
-        <div className="flex ml-8">
-          <div className="flex items-center">
+        <div className="flex flex-1">
+          <nav className="[&>a]:inline-block [&>a]:py-3 [&>a]:px-4 ml-4">
+            <Link href={`/admin/organizations/${id}/general_info`}>
+              General Info
+            </Link>
+            <Link href={`/admin/organizations/${id}/locations`}>Locations</Link>
+          </nav>
+          <div className="flex items-center ml-auto mr-4">
             <Label className="mr-4">Use case</Label>
             <Select value={useCasesQuery.data?.[0]?.id}>
               <SelectTrigger className="w-[180px]">
@@ -46,12 +52,6 @@ const AdminLayout = ({
               </SelectContent>
             </Select>
           </div>
-          <nav className="[&>a]:inline-block [&>a]:py-3 [&>a]:px-4 ml-4">
-            <Link href={`/admin/organizations/${id}/general_info`}>
-              General Info
-            </Link>
-            <Link href={`/admin/organizations/${id}/locations`}>Locations</Link>
-          </nav>
         </div>
       </TitleBar>
       <main className="flex-1 flex flex-col bg-gray-100">{children}</main>
