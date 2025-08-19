688ad2297e2c0b1279cca70f24793cf634ec813c
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-08-19 21:11:07 +0300

    Move organization info under menu

--- app/src/app/admin/organizations/[id]/layout.tsx
+++ app/src/app/admin/organizations/[id]/layout.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import TitleBar from "@/components/title-bar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Label } from "@/components/ui/label";
 import {
   Select,
@@ -11,6 +17,7 @@ import {
 } from "@/components/ui/select";
 import { getUseCases } from "@/services/api";
 import { useQuery } from "@tanstack/react-query";
+import { Ham, HamburgerIcon, LucideHamburger, MenuIcon } from "lucide-react";
 import Link from "next/link";
 import { useParams } from "next/navigation";
 
@@ -32,12 +39,9 @@ const AdminLayout = ({
       <TitleBar toHomeHref={`/admin/organizations/${id}`}>
         <div className="flex flex-1">
           <nav className="[&>a]:inline-block [&>a]:py-3 [&>a]:px-4 ml-4">
-            <Link href={`/admin/organizations/${id}/general_info`}>
-              General Info
-            </Link>
             <Link href={`/admin/organizations/${id}/locations`}>Locations</Link>
           </nav>
-          <div className="flex items-center ml-auto mr-4">
+          <div className="flex items-center ml-auto mr-2">
             <Label className="mr-4">Use case</Label>
             <Select value={useCasesQuery.data?.[0]?.id}>
               <SelectTrigger className="w-[180px]">
@@ -52,6 +56,18 @@ const AdminLayout = ({
               </SelectContent>
             </Select>
           </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="mr-1 px-3">
+              <MenuIcon />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem asChild>
+                <Link href={`/admin/organizations/${id}/general_info`}>
+                  Organization Info
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </TitleBar>
       <main className="flex-1 flex flex-col bg-gray-100">{children}</main>
