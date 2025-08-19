4fdb9d65e995e7c1b24dbb182878b5c7430d4c53
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-08-19 16:23:52 +0300

    Add locations page

--- app/src/app/admin/organizations/[id]/locations/page.tsx
+++ app/src/app/admin/organizations/[id]/locations/page.tsx
@@ -2,9 +2,9 @@ import { PageTemplate } from "@/components/admin/page-template";
 
 const GeneralInfoPage = () => {
   return (
-    <PageTemplate title="General Information">
+    <PageTemplate title="Locations">
       <p className="text-gray-600">
-        This page contains general information about the organization.
+        This page contains info about locations of the organization.
       </p>
     </PageTemplate>
   );
