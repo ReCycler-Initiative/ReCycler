07c0b68efd2cbe00907c60999596c6ceee6dcfc3
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-08-25 12:30:02 +0300

    Show Chatling only in recycler app

--- app/src/app/recycler/layout.tsx
+++ app/src/app/recycler/layout.tsx
@@ -1,3 +1,25 @@
-import layout from "../organizations/layout";
+"use client";
 
-export default layout;
+import Layout from "@/app/organizations/layout";
+import Script from "next/script";
+import { ReactNode } from "react";
+
+const RecyclerLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <Script id="chatling">
+        {`window.chtlConfig = { chatbotId: "3433989154" }`}
+      </Script>
+      <Script
+        async
+        data-id="3433989154"
+        id="chatling-embed-script"
+        type="text/javascript"
+        src="https://chatling.ai/js/embed.js"
+      ></Script>
+      <Layout>{children}</Layout>;
+    </>
+  );
+};
+
+export default RecyclerLayout;
