484af3009c8d258e4ab79bed8e87c429f1439824
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-09-24 08:55:17 +0300

    Fix button

--- app/src/components/ui/button.tsx
+++ app/src/components/ui/button.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
+import { Slot, Slottable } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
@@ -65,7 +65,7 @@ const Button = ({
       type={props.type || "button"}
       {...props}
     >
-      {props.children}
+      <Slottable>{props.children}</Slottable>
       <Loader2Icon className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 hidden animate-spin group-data-[loading=true]:block" />
       <span className="sr-only">Loading...</span>
     </Comp>
