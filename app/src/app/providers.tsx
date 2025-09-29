42829edaac9e010545be28323ca81b4b2f0e8cc9
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-09-29 17:29:40 +0300

    Add toasts for success and error when saving

--- app/src/app/providers.tsx
+++ app/src/app/providers.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <>
+      <Toaster />
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
   );
 };
 
