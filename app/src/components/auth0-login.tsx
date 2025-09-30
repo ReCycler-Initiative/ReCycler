53420968d65909a6fdfa43fcfa48e07cdc1825f5
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-09-30 12:28:20 +0300

    Configure Auth0

--- app/src/components/auth0-login.tsx
+++ app/src/components/auth0-login.tsx
@@ -1,32 +1,31 @@
 "use client";
 
-import { useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0";
+import { Button } from "./ui/button";
+import LoadingSpinner from "./loading-spinner";
 
 export default function Auth0Login() {
   const { user, error, isLoading } = useUser();
 
-  if (isLoading) return <div>Ladataan...</div>;
-  if (error) return <div>Virhe: {error.message}</div>;
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <div>
       {!user ? (
-        <Link
-          href="/api/auth/login"
-          className="px-4 py-2 bg-blue-600 text-white rounded inline-block"
-        >
-          Kirjaudu sisään
-        </Link>
+        <Button asChild>
+          <a href="/auth/login">Kirjaudu sisään</a>
+        </Button>
       ) : (
         <div className="flex items-center gap-2">
           <span>{user.name}</span>
-          <Link
-            href="/api/auth/logout"
-            className="px-3 py-1 bg-gray-700 text-white rounded inline-block"
-          >
-            Kirjaudu ulos
-          </Link>
+          <Button asChild>
+            <a
+              href="/auth/logout"
+            >
+              Kirjaudu ulos
+            </a>
+          </Button>
         </div>
       )}
     </div>
