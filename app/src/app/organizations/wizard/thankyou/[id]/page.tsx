8931a0404773020a3d1dc853b419e29106c89c26
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-10-27 18:48:43 +0200

    Show organization name in the thank you page

--- app/src/app/organizations/wizard/thankyou/[id]/page.tsx
+++ app/src/app/organizations/wizard/thankyou/[id]/page.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import { LoadingState } from "@/components/loading-state";
+import { getOrganizationById } from "@/services/api";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 const ThankYou = () => {
+  const params = useParams();
+  const orgId = params.id as string;
+
+  const {
+    data: organization,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["organization", orgId],
+    queryFn: () => getOrganizationById(orgId),
+  });
+
   return (
-    <div>
-      <h1 className="text-2xl text-center py-6 border-b bg-gray-50 text-primary">
-        Organisaatio luotiin onnistuneesti
-      </h1>
-    </div>
+    <LoadingState isLoading={isLoading} error={isError || !organization}>
+      {organization && <div className="flex-1 bg-white">
+        <h1 className="text-2xl text-center py-6 border-b bg-gray-50 text-primary">
+          Organisaatio {organization.name} luotiin onnistuneesti
+        </h1>
+      </div>}
+    </LoadingState>
   );
 };
 
