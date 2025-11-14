a7aa2a8dd7933733cbb5c09d1fe97a78778de634
Jussi Niilahti <95470287+niilahti@users.noreply.github.com>
2025-11-14 13:58:29 +0200

    Update layout.tsx

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -8,7 +8,7 @@ import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Recycler",
+  title: "Palvelun nimi",
   description: "",
 };
 
