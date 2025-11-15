f808b3a4dd345583be991593fd4f22f4ac93a471
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2025-11-15 07:53:52 +0200

    Fix title bar height

--- app/src/components/title-bar.tsx
+++ app/src/components/title-bar.tsx
@@ -15,7 +15,7 @@ const TitleBar = ({
   toHomeHref?: string;
 }) => {
   return (
-    <header className="pl-1 pr-3 border-b border-gray-400 sticky top-0 bg-white shadow-md z-50 flex items-center justify-between">
+    <header className="h-16 pl-1 pr-3 border-b border-gray-400 sticky top-0 bg-white shadow-md z-50 flex items-center justify-between">
       <div className="flex items-center gap-2 flex-1">
         <Link href={toHomeHref}>
           {logo}
