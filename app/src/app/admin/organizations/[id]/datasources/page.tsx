1f717bf78f5a0992b0dc48148ed86b05f4ff889d
Jussi Niilahti <95470287+niilahti@users.noreply.github.com>
2025-12-12 13:42:55 +0200

    data connector

---
+++ app/src/app/admin/organizations/[id]/datasources/page.tsx
@@ -0,0 +1,12 @@
+import { PageTemplate } from "@/components/admin/page-template";
+
+const DataSourcesPage = () => {
+  return (
+    <PageTemplate title="data sources">
+      <p className="text-gray-600">
+        This page contain Connector creating process and attribute filter.
+      </p>
+    </PageTemplate>
+  );
+};
+export default DataSourcesPage;
