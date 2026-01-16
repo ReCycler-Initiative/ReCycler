9b3af8d6088909879f9ebafd442f4739b0353317
Jussi Niilahti <95470287+niilahti@users.noreply.github.com>
2026-01-16 12:45:13 +0200

    namings

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -7,7 +7,7 @@ import Providers from "./providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kokonaispalvelun nimi",
+  title: "ReCycler Platform",
   description: "",
 };
 
--- app/src/components/title-bar-service.tsx
+++ app/src/components/title-bar-service.tsx
@@ -3,7 +3,7 @@
 import TitleBar from "./title-bar";
 
 const TitleBarService = () => {
-  return <TitleBar logo={<p className="font-bold ml-2">Kokonaispalvelun nimi</p>} toHomeHref="/" />;
+  return <TitleBar logo={<p className="font-bold ml-2">Recycler Platform</p>} toHomeHref="/" />;
 };
 
 export default TitleBarService;
