5e9fdb0cbff1d7ce553c0692cfaca17a071709cb
Pirkka Huhtala <pirkka.huhtala@solita.fi>
2026-02-23 09:21:08 +0200

    Add scaffolding for the customer facing pages

---
+++ app/src/app/organizations/[organizationId]/use_cases/[useCaseId]/materials/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import Container from "@/components/container";
+import { Materials } from "@/components/materials";
+import { Button } from "@/components/ui/button";
+import { Form } from "@/components/ui/form";
+import { useParams } from "next/dist/client/components/navigation";
+import Link from "next/link";
+import { useForm, useWatch } from "react-hook-form";
+
+const MaterialsPage = () => {
+  const params = useParams<{ organizationId: string; useCaseId: string }>();
+  const form = useForm();
+  const materialValues = useWatch({
+    control: form.control,
+    name: "materials",
+    defaultValue: {},
+  });
+  const materials: [string, boolean][] = Object.entries(materialValues);
+  const selectedMaterials = materials.filter(([, value]) => value);
+
+  return (
+    <Form {...form}>
+      <Container className="max-w-2xl pt-7 lg:pt-14">
+        <h1 className="text-xl font-medium mb-4 font-sans">
+          Mitäs tänään kierrätetään?
+        </h1>
+        <div className="mb-28 lg:mb-6">
+          <Materials />
+        </div>
+      </Container>
+      <div className="fixed lg:static bottom-0 bg-white lg:bg-transparent border lg:border-none p-4 lg:p-0 left-0 right-0 border-gray-400 flex flex-col items-center gap-y-4">
+        Materiaaleja valittu {selectedMaterials.length} kpl
+        <Button asChild className="w-full max-w-96 lg:mb-6" size="lg">
+          <Link
+            href={`/organizations/${params.organizationId}/use_cases/${params.useCaseId}/results?materials=${encodeURIComponent(
+              selectedMaterials.map(([key]) => key).join(",")
+            )}`}
+          >
+            Näytä kierrätyspisteet
+          </Link>
+        </Button>
+      </div>
+    </Form>
+  );
+};
+
+export default MaterialsPage;
---
+++ app/src/app/organizations/[organizationId]/use_cases/[useCaseId]/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Container from "@/components/container";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+//import hero from "./recycle.png"; //let´s place when suitable photo
+
+const HomePage = () => {
+  const params = useParams<{ organizationId: string; useCaseId: string }>();
+
+  return (
+    <>
+      {/*<div className="h-48 bg-gray-400 w-full border-b border-b-gray-400">
+        <Image
+          alt=""
+          className="object-cover w-full h-full object-center"
+          src={hero}
+      </div>
+         */}
+      <Container className="flex-1 max-w-4xl">
+        <h1 className="text-2xl font-medium mb-4 font-sans">
+          Tervetuloa ReCycleriin
+        </h1>
+        <p className="mb-12 font-sans">
+          Tervetuloa ReCycler-sovellukseen! ReCycler auttaa sinua löytämään
+          helposti lähimmät kierrätyspisteet kotitalousjätteillesi kaikkialla
+          Suomessa. Olipa kyseessä mikä tahansa kierrätysmateriaali, ReCycler
+          opastaa sinut oikeaan paikkaan. Tee ympäristöystävällisiä päätöksiä jo
+          tänään!
+        </p>
+        <div className="flex flex-col gap-4 items-center">
+          <Button className="w-full max-w-96" asChild size="lg">
+            <Link
+              href={`/organizations/${params.organizationId}/use_cases/${params.useCaseId}/materials`}
+            >
+              Lähde kierrättämään
+            </Link>
+          </Button>
+          <Button className="w-full max-w-96" asChild variant="secondary">
+            <Link className="no-underline" href="/recycler/results">
+              Näytä lähimmät kierrätyspisteet
+            </Link>
+          </Button>
+        </div>
+      </Container>
+      {/* Footer */}
+      <footer className="w-full mt-16 bg-gray-200 py-4 text-center">
+        <p className="text-xs">
+          ReCycler on Github-projekti. Lähdekoodi ja lisenssitiedot löytyvät
+          projektin{" "}
+          <a
+            href="https://github.com/ReCycler-Initiative/ReCycler"
+            className="text-blue-500 underline"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github-sivuilta
+          </a>
+          . Projekti on lisensoitu Apache 2.0 -lisenssillä. Projektissa käytetty
+          kierrätyspisteiden sijainti- ja ominaisuustiedot ovat peräisin
+          Kierrätys.info-palvelun rajapinnasta (API). Kaikki oikeudet
+          Kierrätys.info-palveluun ja sen dataan omistaa KIVO ry.
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default HomePage;
